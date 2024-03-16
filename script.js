84251562e6ea415e2cf2354a0c05ee0d3b4df01d
Rama_Prakash <ramaprakeshmanugula@gmail.com>
2024-03-16 14:55:36 +0530

    Moving files to github

---
+++ script.js
@@ -0,0 +1,49 @@
+const api = {
+    key: "fcc8de7015bbb202209bbf0261babf4c",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const search = document.getElementById('search');
+search.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(search.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(display);
+}
+
+function display(weather) {
+    let city = document.querySelector('.location');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let temp = document.querySelector('.temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    let now = new Date();
+    let date = document.querySelector('.date');
+    date.innerText = date1(now);
+
+    let weather1 = document.querySelector('.current');
+    weather1.innerText = weather.weather[0].main;
+
+   
+}
+
+function date1(d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
